0b183143897a11a266f0ddbe46014baf27de996c
rgrace9 <rgracewilliams@gmail.com>
2021-10-02 23:19:23 -0400

    Remove unnecessary files.

---
+++ src/lib/Autocomplete/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Autocomplete = props => {
+  return (
+    <div>
+      <p>Autocomplete</p>
+    </div>
+  );
+};
+
+Autocomplete.propTypes = {
+  
+};
+
+export default Autocomplete;
